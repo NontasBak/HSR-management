8933ac6d6925de4bcac5e62980fd39577f9fad4a
NontasBak <nontasbak@gmail.com>
2025-03-02 15:57:30 +0200

    Fetch data from API and add it to the database

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = new Pool({
   host: "localhost",
-  user: process.env.ROLE_USER,
+  user: process.env.ROLE_NAME,
   database: "hsr_management",
   password: process.env.ROLE_PASSWORD,
   port: 5432
--- db/populate.js
+++ db/populate.js
@@ -2,30 +2,111 @@
 
 const { Client } = require("pg");
 require("dotenv").config();
+const axios = require("axios");
 
-const SQL = `
-CREATE TABLE IF NOT EXISTS characters (
-    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR ( 255 ),
-    element VARCHAR ( 255 ),
-    path_id INTEGER REFERENCES path ( id )
-);
+async function createDB() {
+    const client = new Client({
+        connectionString: `postgresql://${process.env.ROLE_NAME}:${process.env.ROLE_PASSWORD}@localhost:5432/hsr_management`,
+    });
 
-CREATE TABLE IF NOT EXISTS path (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR ( 255 )
-);
-`;
+    try {
+        const SQL = `
+            CREATE TABLE IF NOT EXISTS path (
+                id VARCHAR ( 255 ) PRIMARY KEY,
+                text VARCHAR ( 255 )
+            );
+
+            CREATE TABLE IF NOT EXISTS characters (
+                id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                name VARCHAR ( 255 ),
+                element VARCHAR ( 255 ),
+                path_id VARCHAR ( 255 ) REFERENCES path ( id ),
+                image VARCHAR ( 255 )
+            );
+        `;
+
+        await client.connect();
+        await client.query(SQL);
+    } catch (error) {
+        console.error("Error creating tables:", error);
+    } finally {
+        await client.end();
+    }
+}
+
+async function getPaths() {
+    const response = await axios.get(
+        "https://vizualabstract.github.io/StarRailStaticAPI/db/en/paths.json"
+    );
+    const data = await response.data;
+
+    const client = new Client({
+        connectionString: `postgresql://${process.env.ROLE_NAME}:${process.env.ROLE_PASSWORD}@localhost:5432/hsr_management`,
+    });
+
+    try {
+        await client.connect();
+
+        for (const [key, path] of Object.entries(data)) {
+            const query = {
+                text: "INSERT INTO path (id, text) VALUES ($1, $2)",
+                values: [path.id, path.text],
+            };
+
+            await client.query(query);
+            console.log(`Inserted path ${path.id}`);
+        }
+    } catch (error) {
+        console.error("Error inserting paths:", error);
+    } finally {
+        await client.end();
+    }
+}
+
+async function getCharacters() {
+    const BASE_IMAGE_URL =
+        "https://vizualabstract.github.io/StarRailStaticAPI/assets/image/character_portrait";
+    const response = await axios.get(
+        "https://vizualabstract.github.io/StarRailStaticAPI/db/en/characters.json"
+    );
+    const data = await response.data;
+
+    const client = new Client({
+        connectionString: `postgresql://${process.env.ROLE_NAME}:${process.env.ROLE_PASSWORD}@localhost:5432/hsr_management`,
+    });
+
+    try {
+        await client.connect();
+
+        for (const [key, character] of Object.entries(data)) {
+            if(character.name == "{NICKNAME}") {
+                character.name = character.tag.startsWith("playergirl") ? "Stelle" : "Caelus";
+            }
+            const query = {
+                text: "INSERT INTO characters (name, element, path_id, image) VALUES ($1, $2, $3, $4)",
+                values: [
+                    character.name,
+                    character.element,
+                    character.path,
+                    `${BASE_IMAGE_URL}/${character.id}.png`,
+                ],
+            };
+
+            await client.query(query);
+            console.log(`Inserted character ${character.name}`);
+        }
+    } catch (error) {
+        console.error("Error inserting characters:", error);
+    } finally {
+        await client.end();
+    }
+}
 
 async function main() {
-  console.log("seeding...");
-  const client = new Client({
-    connectionString: `postgresql://${process.env.ROLE_USER}:${process.env.ROLE_PASSWORD}@localhost:5432/hsr_management`,
-  });
-  await client.connect();
-  await client.query(SQL);
-  await client.end();
-  console.log("done");
+    await createDB();
+    await getPaths();
+    await getCharacters();
+    console.log("done");
 }
 
 main();
